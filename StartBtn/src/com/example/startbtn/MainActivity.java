ea347a5070710fe21691fb7acd7d9b8f2f98ddd1
RyanGoldsberry <sj.kingrg@gmail.com>
2014-09-23 15:48:43 -0400

    new

---
+++ StartBtn/src/com/example/startbtn/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.startbtn;
+
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
+
+
+public class MainActivity extends ActionBarActivity {
+	
+	Button btn;
+	
+	
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = (Button)findViewById(R.id.btnNext);
+        btn.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent (MainActivity.this,SecondActivity.class);
+				startActivity(intent);
+			};
+        });
+    };
+}
+
+        
+    
+
