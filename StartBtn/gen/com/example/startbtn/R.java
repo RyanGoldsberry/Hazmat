ec45aa65acd9a7ff89c1b0bf5e425936744ab3e0
RyanGoldsberry <sj.kingrg@gmail.com>
2014-09-24 13:11:36 -0400

    1st Reprository

--- StartBtn/gen/com/example/startbtn/R.java
+++ StartBtn/gen/com/example/startbtn/R.java
@@ -1077,7 +1077,9 @@ containing a value of this type.
         public static final int abc_textfield_searchview_holo_light=0x7f020054;
         public static final int abc_textfield_searchview_right_holo_dark=0x7f020055;
         public static final int abc_textfield_searchview_right_holo_light=0x7f020056;
-        public static final int ic_launcher=0x7f020057;
+        public static final int hazmat_mgn=0x7f020057;
+        public static final int ic_launcher=0x7f020058;
+        public static final int splash=0x7f020059;
     }
     public static final class id {
         public static final int action_bar=0x7f05001c;
@@ -1243,6 +1245,7 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a000f;
         public static final int app_name=0x7f0a000d;
+        public static final int btn1=0x7f0a0013;
         public static final int btn_second_title=0x7f0a0012;
         public static final int btn_title=0x7f0a0011;
         public static final int hello_world=0x7f0a000e;
--- StartBtn/src/com/example/startbtn/SecondActivity.java
+++ StartBtn/src/com/example/startbtn/SecondActivity.java
@@ -1,6 +1,9 @@
 package com.example.startbtn;
 
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.*;
+import android.content.Intent;
 import android.os.Bundle;
 
 
@@ -8,10 +11,20 @@ import android.os.Bundle;
 
 public class SecondActivity extends ActionBarActivity {
 
+	Button btn;
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second);
-    }
+        btn = (Button)findViewById(R.id.btnEnd);
+        btn.setOnClickListener(new View.OnClickListener() {
+        	
+        	public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent = new Intent (SecondActivity.this,MainActivity.class);
+				startActivity(intent);
+        };
+    });
 
-}
+}};
